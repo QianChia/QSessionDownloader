28977bd35d73a8e4d59b39746366e8a827bbcd96
仟 <jhqian0228@icloud.com>
2016-07-15 02:48:36 +0800

    chore: 更新测试方法

--- QSessionDownloaderExample/ViewController.m
+++ QSessionDownloaderExample/ViewController.m
@@ -22,127 +22,94 @@
 
 @implementation ViewController
 
-- (IBAction)start1:(UIButton *)sender {
-    
-    self.progressBtn1 = sender;
-    [self startDownload1:sender];
-}
+/// 开始下载
 
-- (IBAction)goon1 {
-    
-    [self startDownload1:self.progressBtn1];
-}
-
-- (IBAction)pause1 {
-    
-    [self pauseDownload1];
-}
-
-- (IBAction)cancel1 {
-    
-    [self cancelDownload1];
-}
-
-// 开始下载
-
-- (void)startDownload1:(UIButton *)sender {
-    
-    NSURL *url = [NSURL URLWithString:@"http://dldir1.qq.com/qqfile/QQforMac/QQ_V4.0.2.dmg"];
-    self.url1 = url;
+- (void)startDownloadWithURL:(NSURL *)url button:(UIButton *)button {
     
     [[QDownloaderManager sharedManager] q_downloadWithURL:url progress:^(float progress) {
         
-        dispatch_sync(dispatch_get_main_queue(), ^{
-            [self.progressBtn1 q_setButtonWithProgress:progress lineWidth:10 lineColor:nil backgroundColor:[UIColor yellowColor]];
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [button q_setButtonWithProgress:progress lineWidth:10 lineColor:nil backgroundColor:[UIColor yellowColor]];
         });
         
     } successed:^(NSString *targetPath) {
         
-        NSLog(@"%@", targetPath);
+        NSLog(@"文件下载成功：%@", targetPath);
         
     } failed:^(NSError *error) {
         
-        NSLog(@"%@", error);
+        NSLog(@"文件下载失败：%@", error);
     }];
 }
 
-// 暂停下载
+/// 暂停下载
 
-- (void)pauseDownload1 {
+- (void)pauseDownloadWithURL:(NSURL *)url {
     
-    [[QDownloaderManager sharedManager] q_pauseWithURL:self.url1];
+    [[QDownloaderManager sharedManager] q_pauseWithURL:url];
 }
 
-// 取消下载
+/// 取消下载
 
-- (void)cancelDownload1{
+- (void)cancelDownloadWithURL:(NSURL *)url button:(UIButton *)button {
     
-    [[QDownloaderManager sharedManager] q_cancelWithURL:self.url1];
+    [[QDownloaderManager sharedManager] q_cancelWithURL:url];
     
-    [self.progressBtn1 q_setButtonWithProgress:0 lineWidth:10 lineColor:nil backgroundColor:[UIColor yellowColor]];
+    [button q_setButtonWithProgress:0 lineWidth:10 lineColor:nil backgroundColor:[UIColor yellowColor]];
+}
+
+/******************************************************************************************************************************/
+
+- (IBAction)start1:(UIButton *)sender {
+    
+    NSURL *url = [NSURL URLWithString:@"http://dldir1.qq.com/qqfile/QQforMac/QQ_V4.0.2.dmg"];
+    
+    self.url1 = url;
+    self.progressBtn1 = sender;
+    
+    [self startDownloadWithURL:url button:sender];
+}
+
+- (IBAction)goon1 {
+    
+    [self startDownloadWithURL:self.url1 button:self.progressBtn1];
+}
+
+- (IBAction)pause1 {
+    
+    [self pauseDownloadWithURL:self.url1];
+}
+
+- (IBAction)cancel1 {
+    
+    [self cancelDownloadWithURL:self.url1 button:self.progressBtn1];
 }
 
 /******************************************************************************************************************************/
 
 - (IBAction)start2:(UIButton *)sender {
     
+    NSURL *url = [NSURL URLWithString:@"http://120.25.226.186:32812/resources/videos/minion_01.mp4"];
+    
+    self.url2 = url;
     self.progressBtn2 = sender;
-    [self startDownload2:sender];
+    
+    [self startDownloadWithURL:url button:sender];
 }
 
 - (IBAction)goon2 {
     
-    
-    [self startDownload2:self.progressBtn2];
+    [self startDownloadWithURL:self.url2 button:self.progressBtn2];
 }
 
 - (IBAction)pause2 {
     
-    [self pauseDownload2];
+    [self pauseDownloadWithURL:self.url2];
 }
 
 - (IBAction)cancel2 {
     
-    [self cancelDownload2];
-}
-
-// 开始下载
-
-- (void)startDownload2:(UIButton *)sender {
-    
-    NSURL *url = [NSURL URLWithString:@"http://120.25.226.186:32812/resources/videos/minion_01.mp4"];
-    self.url2 = url;
-    
-    [[QDownloaderManager sharedManager] q_downloadWithURL:url progress:^(float progress) {
-        
-        dispatch_sync(dispatch_get_main_queue(), ^{
-            [self.progressBtn2 q_setButtonWithProgress:progress lineWidth:10 lineColor:nil backgroundColor:[UIColor yellowColor]];
-        });
-        
-    } successed:^(NSString *targetPath) {
-        
-        NSLog(@"%@", targetPath);
-        
-    } failed:^(NSError *error) {
-        
-        NSLog(@"%@", error);
-    }];
-}
-
-// 暂停下载
-
-- (void)pauseDownload2 {
-    
-    [[QDownloaderManager sharedManager] q_pauseWithURL:self.url2];
-}
-
-// 取消下载
-
-- (void)cancelDownload2{
-    
-    [[QDownloaderManager sharedManager] q_cancelWithURL:self.url2];
-    
-    [self.progressBtn2 q_setButtonWithProgress:0 lineWidth:10 lineColor:nil backgroundColor:[UIColor yellowColor]];
+    [self cancelDownloadWithURL:self.url2 button:self.progressBtn2];
 }
 
 @end
